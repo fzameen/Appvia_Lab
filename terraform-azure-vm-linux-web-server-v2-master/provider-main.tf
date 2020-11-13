61ae4fc8a894623d9c956540e6231028a37fa734
fzameen <43372169+fzameen@users.noreply.github.com>
2020-11-13 19:06:58 +0000

    Update provider-main.tf

--- terraform-azure-vm-linux-web-server-v2-master/provider-main.tf
+++ terraform-azure-vm-linux-web-server-v2-master/provider-main.tf
@@ -13,11 +13,3 @@ provider "azurerm" {
   version = ">= 2.0.0"
   features {}  
 }
-#Configure the Azure Provider
-provider "azurerm" {
-  environment = "public"
-  subscription_id = var.azure_subscription_id
-  client_id = var.azure_client_id
-  client_secret = var.azure_client_secret
-  tenant_id = var.azure_tenant_id
-}

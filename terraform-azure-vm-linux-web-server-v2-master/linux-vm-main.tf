628ae7d2390a1c967044dcef82de00e4f0fb234d
fzameen <43372169+fzameen@users.noreply.github.com>
2020-11-13 19:13:14 +0000

    Update linux-vm-main.tf

--- terraform-azure-vm-linux-web-server-v2-master/linux-vm-main.tf
+++ terraform-azure-vm-linux-web-server-v2-master/linux-vm-main.tf
@@ -1,7 +1,14 @@
 ###########################
 ## Azure Linux VM - Main ##
 ###########################
-
+#Configure the Azure Provider
+provider "azurerm" {
+  environment = "public"
+  subscription_id = var.azure_subscription_id
+  client_id = var.azure_client_id
+  client_secret = var.azure_client_secret
+  tenant_id = var.azure_tenant_id
+}
 # Generate random password
 resource "random_password" "linux-vm-password" {
   length           = 16

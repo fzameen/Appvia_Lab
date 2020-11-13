bdeca166325106c93a43f52ecf5758c3d78e4a79
fzameen <43372169+fzameen@users.noreply.github.com>
2020-11-13 19:07:24 +0000

    Update provider-variables.tf

--- terraform-azure-vm-linux-web-server-v2-master/provider-variables.tf
+++ terraform-azure-vm-linux-web-server-v2-master/provider-variables.tf
@@ -23,3 +23,11 @@ variable "azure-tenant-id" {
   type        = string
   description = "Azure Tenant ID"
 }
+#Configure the Azure Provider
+provider "azurerm" {
+  environment = "public"
+  subscription_id = var.azure_subscription_id
+  client_id = var.azure_client_id
+  client_secret = var.azure_client_secret
+  tenant_id = var.azure_tenant_id
+}

888b7dc5cc1f9677c4ce941ebacad42e074271cb
fzameen <43372169+fzameen@users.noreply.github.com>
2020-11-14 20:46:06 +0000

    Update provider-main.tf

--- terraform-azure-vm-linux-web-server-v2-master/provider-main.tf
+++ terraform-azure-vm-linux-web-server-v2-master/provider-main.tf
@@ -3,6 +3,11 @@
 ## Azure Provider - Main ##
 ###########################
 
+#Define Terraform provider
+terraform {
+required_version = ">= 0.12"
+}
+
 # Configure the Azure provider
 provider "azurerm" { 
  environment = "public"

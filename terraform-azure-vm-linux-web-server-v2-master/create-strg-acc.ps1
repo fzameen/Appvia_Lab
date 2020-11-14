7652bb46b05618b014679f11b164b62b408ba331
fzameen <43372169+fzameen@users.noreply.github.com>
2020-11-14 17:26:51 +0000

    Update create-strg-acc.ps1

--- terraform-azure-vm-linux-web-server-v2-master/create-strg-acc.ps1
+++ terraform-azure-vm-linux-web-server-v2-master/create-strg-acc.ps1
@@ -5,4 +5,4 @@ New-AzResourceGroup -Name "rg-terrafrom" -Location "westeurope"
 New-AzStorageAccount -ResourceGroupName "rg-terrafrom" -AccountName "strg1terraform01" -Location westeurope -SkuName Standard_LRS
  
 #Create Storage Container
-New-AzStorageContainer -ResourceGroupName "rg-terrafrom" -AccountName "strg1terraform01" -ContainerName "cntr1terrafrom"
+New-AzContainer -ResourceGroupName "rg-terrafrom" -AccountName "strg1terraform01" -ContainerName "cntr1terrafrom"

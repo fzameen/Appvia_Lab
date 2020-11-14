fc76cdca92dbf83c3dff59150cea3cf172debf79
fzameen <43372169+fzameen@users.noreply.github.com>
2020-11-14 18:28:51 +0000

    Update create-strg-acc.ps1

--- terraform-azure-vm-linux-web-server-v2-master/create-strg-acc.ps1
+++ terraform-azure-vm-linux-web-server-v2-master/create-strg-acc.ps1
@@ -5,4 +5,15 @@ New-AzResourceGroup -Name "rg-terrafrom" -Location "westeurope"
 New-AzStorageAccount -ResourceGroupName "rg-terrafrom" -AccountName "strg1terraform01" -Location westeurope -SkuName Standard_LRS
  
 #Create Storage Container
-New-AzContainer -ResourceGroupName "rg-terrafrom" -AccountName "strg1terraform01" -ContainerName "cntr1terrafrom"
+# New-AzStorageContainer -ResourceGroupName "rg-terrafrom" -AccountName "strg1terraform01" -ContainerName "cntr1terrafrom"
+# Retrieve an existing Storage Account reference
+#$storageContext = Get-AzStorageAccount -ResourceGroupName rg-terrafrom `
+  #  -Name build5ninesblobs
+
+# Retrieve the Context from the Storage Account
+$storageContext = $storageAccount.Context
+
+#create storage container
+New-AzStorageContainer -Name "cntr1terrafrom"
+    -Context $storageContext
+    -Permission Off

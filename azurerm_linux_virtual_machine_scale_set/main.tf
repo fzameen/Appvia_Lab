2ad0b1c2ed0a87f816467f7a336e2a1fcc4de7d5
fzameen <43372169+fzameen@users.noreply.github.com>
2020-11-14 22:33:09 +0000

    Update main.tf

--- azurerm_linux_virtual_machine_scale_set/main.tf
+++ azurerm_linux_virtual_machine_scale_set/main.tf
@@ -1,5 +1,14 @@
 provider "azurerm" {
+  version = "=2.0"
   features {}
+}
+
+terraform {
+    backend "azurerm" {
+        resource_group_name = "rg-terraform"   
+        storage_account_name = "strg1terraform01"
+        container_name = "cntr1terrafrom"
+    }
 }
 
 resource "azurerm_resource_group" "example" {

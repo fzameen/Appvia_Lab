4c85589bd1d86687e2ed80f452b22695809ca3e6
fzameen <43372169+fzameen@users.noreply.github.com>
2020-11-14 21:42:53 +0000

    Update provider-main.tf

--- terraform-azure-vm-linux-web-server-v2-master/provider-main.tf
+++ terraform-azure-vm-linux-web-server-v2-master/provider-main.tf
@@ -8,13 +8,18 @@ terraform {
 required_version = ">= 0.12"
 }
 
+$ export ARM_CLIENT_ID="41da9c40-91c7-4c82-b48d-d8be59a5219b"
+$ export ARM_CLIENT_SECRET="CMELrBl26Lc11CC8kAJs~-s~~8D_6ng3uD"
+$ export ARM_SUBSCRIPTION_ID="d7879dc0-f991-4e28-8e0c-55a9e9d63635"
+$ export ARM_TENANT_ID="43fd6cf5-e8be-49d0-9d6a-68fe11cb4e59"
+
 # Configure the Azure provider
 provider "azurerm" { 
  environment = "public"
-	azure_subscription_id = "d7879dc0-f991-4e28-8e0c-55a9e9d63635"
-	azure_client_id = "41da9c40-91c7-4c82-b48d-d8be59a5219b"
-	azure_client_secret = "CMELrBl26Lc11CC8kAJs~-s~~8D_6ng3uD"
-azure_tenant_id = "43fd6cf5-e8be-49d0-9d6a-68fe11cb4e59"
+#	azure_subscription_id = "d7879dc0-f991-4e28-8e0c-55a9e9d63635"
+#	azure_client_id = "41da9c40-91c7-4c82-b48d-d8be59a5219b"
+#	azure_client_secret = "CMELrBl26Lc11CC8kAJs~-s~~8D_6ng3uD"
+#	azure_tenant_id = "43fd6cf5-e8be-49d0-9d6a-68fe11cb4e59"
 version = ">= 2.0.0"
 features {}  
  }

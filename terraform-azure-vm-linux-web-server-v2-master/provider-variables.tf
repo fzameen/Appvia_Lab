2dee61c727fa2469c406ac307fe0d122e99d44b1
fzameen <43372169+fzameen@users.noreply.github.com>
2020-11-14 19:24:02 +0000

    Update provider-variables.tf

--- terraform-azure-vm-linux-web-server-v2-master/provider-variables.tf
+++ terraform-azure-vm-linux-web-server-v2-master/provider-variables.tf
@@ -23,8 +23,3 @@ variable "azure-tenant-id" {
   type        = string
   description = "Azure Tenant ID"
 }
-	#Azure authentication variables
-	azure_subscription_id = "d7879dc0-f991-4e28-8e0c-55a9e9d63635"
-	azure_client_id = "41da9c40-91c7-4c82-b48d-d8be59a5219b"
-	azure_client_secret = "CMELrBl26Lc11CC8kAJs~-s~~8D_6ng3uD"
-azure_tenant_id = "43fd6cf5-e8be-49d0-9d6a-68fe11cb4e59"

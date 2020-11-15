8f33cfbbb5ff5c0726bc121841ed4958f10ddbef
fzameen <43372169+fzameen@users.noreply.github.com>
2020-11-15 10:22:26 +0000

    Update main.tf

--- azurerm_linux_virtual_machine_scale_set/main.tf
+++ azurerm_linux_virtual_machine_scale_set/main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "=2.13.0"
+  version = "=2.0"
   features {}
 }
 
@@ -13,15 +13,15 @@ terraform {
 
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_resource_group" "terraformrg" {
-  name     = "rg-terraform-01"
-  location = "westeurope"
+resource "azurerm_resource_group" "tamopsrg" {
+  name     = "tamops-tf"
+  location = "eastus2"
 }
  
-resource "azurerm_storage_account" "strg1terraform" {
-  name                     = "strg1terraform01"
-  resource_group_name      = azurerm_resource_group.terraformrg.name
-  location                 = azurerm_resource_group.terraformrg.location
+resource "azurerm_storage_account" "tamopssa" {
+  name                     = "tamopssatf"
+  resource_group_name      = azurerm_resource_group.tamopsrg.name
+  location                 = azurerm_resource_group.tamopsrg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }

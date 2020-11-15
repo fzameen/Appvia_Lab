421c1f7b310e9a9564823934f79a77f121c0d8dc
fzameen <43372169+fzameen@users.noreply.github.com>
2020-11-15 22:58:18 +0000

    Update sqlha.tf

--- azure_sql_ha/sqlha.tf
+++ azure_sql_ha/sqlha.tf
@@ -19,7 +19,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_sql_server" "example" {
-  name                         = "my-sql-server"
+  name                         = "my-sql-esserv"
   resource_group_name          = azurerm_resource_group.example.name
   location                     = azurerm_resource_group.example.location
   version                      = "12.0"
